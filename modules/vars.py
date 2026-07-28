7091e6dec77c4d2289718bb973741238c9780181
Jackie547 <holarick97@gmail.com>
2026-07-28 10:44:34 +0530

    Set default values for api_id and api_hash
    Updated api_id and api_hash with default values.

--- modules/vars.py
+++ modules/vars.py
@@ -1,7 +1,7 @@
 from os import getenv
 
-api_id    = int(getenv("api_id", ""))
-api_hash  = getenv("api_hash", "")
+api_id    = int(getenv("api_id", "31605369"))
+api_hash  = getenv("api_hash", "f8b54f0c81481ebf1593adebb073d844")
 bot_token = getenv("bot_token", "")
 pwtoken = getenv("pwtoken", "")
 CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
